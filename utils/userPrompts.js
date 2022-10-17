8decd1c71afac9b4703580cb10735fd31b411a5d
William Chalk <william@printedproducts.com>
2022-10-17 16:53:28 -0400

    updating functions

--- utils/userPrompts.js
+++ utils/userPrompts.js
@@ -1,4 +1,4 @@
-const db = require('mysql2');
+const mysql = require('mysql2');
 const {prompt} = require('inquirer');
 
 const db = mysql
@@ -14,32 +14,38 @@ const db = mysql
   .promise();
 
 const viewEmployees = async () => {
-    const [employeeData] = await db.query(`SELECT * FROM employee`);
-    console.table(employeeData);
-    mainMenu();
+    db.query("SELECT employee.first_name, employee.last_name, employee_role.role_title, employee_role.role_salary, department.name, CONCAT(e.first_name, ' ' ,e.last_name) AS Manager FROM employee INNER JOIN employee_role on employee_role.id = employee.role_id INNER JOIN department on department.id = employee_role.department_id left join employee e on employee.manager_id = e.id;",(err,res)=>{
+        if(err) throw err;
+        console.table(res);
+        mainMenu();
+    });
   };
   
   const viewDepartments = async () => {
-    const [departmentData] = await db.query(`SELECT * FROM deparment`);
-    console.table(departmentData);
-    mainMenu();
+    db.query("SELECT employee.first_name, employee.last_name, department.name AS Department FROM employee JOIN employee_role ON employee.role_id = employee_role.id JOIN department ON employee_role.department_id = department.id ORDER BY employee.id;",(err,res)=>{
+        if(err) throw err;
+        console.table(res);
+        mainMenu();
+    });
   };
   
   const viewRoles = async () =>{
-    const [roleData] = await db.query('SELECT * FROM employee_role');
-    console.table(roleData);
-    mainMenu();
+    db.query("SELECT employee.first_name, employee.last_name, employee_role.title AS Title FROM employee JOIN role ON employee.role_id = employee_role.id;",(err,res)=>{
+        if(err) throw err;
+        console.table(res);
+        mainMenu();
+    });
   }
   
   const addDepartment = () =>{
-   const userInput = prompt[
+   const userInput = prompt([
       {
         type: 'input',
         name: 'Department',
         message: 'Please enter the new Department'
         //add validation
       }
-    ]
+    ])
   
     db.query(`INSERT INTO department (department_name) VALUES ${userInput}`);
     const deparmentData = db.query(`SELECT * FROM department`);
@@ -48,7 +54,7 @@ const viewEmployees = async () => {
   
   const addRole = () => {
     //Enter name salary and department
-    const userInput = prompt[
+    const userInput = prompt([
       {
         type: 'input',
         name: 'role title',
@@ -67,15 +73,58 @@ const viewEmployees = async () => {
         message: 'Please enter the Department ID',
         value: 'department_id'
       }
-    ]
+    ])
   
     db.query(`INSERT INTO employee_role (role_title,role_salary,department_id) VALUES ${userInput}`);
     const roleData = db.query(`SELECT * FROM employee_role`);
-    console.log(roleData);
+    console.table(roleData);
   }
   
   const addEmployee = () =>{
     //Enter first_name last_name role and manager
+    const userInput = prompt([
+        {
+            type:'input',
+            name: 'firstName',
+            message: "Please type employee's first name.",
+            value:'first_name'
+        },
+        {
+            type: 'input',
+            name: 'lastName',
+            message: "Please type the employee's last name",
+            value: 'last_name'
+        },
+        {
+            type: 'list',
+            name:'roleAssign',
+            message: "Please select the employee's role",
+            choices: listRoles()
+        },
+        {
+            type: 'list',
+            name: 'managerID',
+            message: "Please select the employee's manager",
+            choices: listManagers()
+        }
+    ]).then((val)=>{
+        const roleID = listRoles().indexOf(val.role) + 1;
+
+        db.query(`UPDATE employee SET WHERE id = ?`,{
+            last_name: val.lastName
+        },
+        {
+            role_id: roleID
+        },
+        function(err){
+            if(err) throw err;
+            console.table(val);
+            mainMenu();
+        }
+        );
+
+    });
+
   }
   
   const updateRole = () =>{
@@ -84,6 +133,17 @@ const viewEmployees = async () => {
     //Show list of Roles
     //Option to update salary
     //Option to update manager
+  }
+
+  const roleArr = [];
+  const listRoles = () =>{
+    db.query(`SELECT * FROM employee_role`,(err,res)=>{
+        if(err) throw err;
+        for(let i = 0; i<res.length;i++){
+            roleArr.push(res[i].role_title);
+        }
+    });
+    return roleArr; 
   }
 
   module.exports = {
